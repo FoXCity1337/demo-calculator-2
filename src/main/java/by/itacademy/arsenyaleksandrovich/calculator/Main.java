2b481b73e96f5c08defe144b593ee9b92b07760a
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-17 22:44:23 +0300

    Создание метода Main, который заполняет поля на https://svyatoslav.biz/testlab/wt/index.php

---
+++ src/main/java/by/itacademy/arsenyaleksandrovich/calculator/Main.java
@@ -0,0 +1,41 @@
+package by.itacademy.arsenyaleksandrovich.calculator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.navigate().to("https://svyatoslav.biz/testlab/wt/index.php");
+
+        String xpath ="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By by = By.xpath(xpath);
+        WebElement webElement = webDriver.findElement(by);
+        webElement.sendKeys("Арсений");
+
+        xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        by = By.xpath(xpath);
+        webElement = webDriver.findElement(by);
+        webElement.sendKeys("185");
+
+        xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        by = By.xpath(xpath);
+        webElement = webDriver.findElement(by);
+        webElement.sendKeys("65");
+
+        xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        by = By.xpath(xpath);
+        webElement = webDriver.findElement(by);
+        webElement.click();
+
+        xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        by = By.xpath(xpath);
+        webElement = webDriver.findElement(by);
+        webElement.click();
+    }
+}
