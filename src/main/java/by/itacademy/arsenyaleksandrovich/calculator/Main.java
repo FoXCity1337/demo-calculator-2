350c7a7a3926650cb7d17f5f89f133c92bafb3cd
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-17 22:52:23 +0300

    Создание метода Main, который заполняет поля на https://svyatoslav.biz/testlab/wt/index.php

--- src/main/java/by/itacademy/arsenyaleksandrovich/calculator/Main.java
+++ src/main/java/by/itacademy/arsenyaleksandrovich/calculator/Main.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 public class Main {
     public static void main(String[] args) {
         WebDriver webDriver = new FirefoxDriver();
