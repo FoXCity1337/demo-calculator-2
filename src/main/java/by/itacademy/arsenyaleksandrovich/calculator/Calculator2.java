abe44e374e0c9b9cd49432ccbebf88a66f9159a8
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-17 20:32:34 +0300

    Создание тестового метода testSum

--- src/main/java/by/itacademy/arsenyaleksandrovich/calculator/Calculator2.java
+++ src/main/java/by/itacademy/arsenyaleksandrovich/calculator/Calculator2.java
@@ -2,7 +2,7 @@ package by.itacademy.arsenyaleksandrovich.calculator;
 
 public class Calculator2 {
 
-    public static double sum(double a, double b) {
+    public double sum(double a, double b) {
         return a + b;
     }
 }
